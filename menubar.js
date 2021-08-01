b56393e74bfb2e2a175461af3c3f1b9635f9895f
salekin-techforing <88052267+salekin-techforing@users.noreply.github.com>
2021-08-01 18:42:23 +0600

    Update menubar.js

--- menubar.js
+++ menubar.js
@@ -1,4 +1,4 @@
-let menuItemsName = ['dashboard', 'services', 'orders', 'subscriptions', 'events', 'notifications', 'settings'];
+let menuItemsName = ['dashboard.html', 'services', 'orders', 'subscriptions', 'events', 'notifications', 'settings'];
 let url = window.location.href;
 
 menuItemsName.forEach((item, index) => {
